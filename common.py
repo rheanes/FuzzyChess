ddcda772ec3791bc3c0332df85fb76d781bb15e2
roberteanes <rhilleanes@gmail.com>
2022-01-28 18:26:40 -0500

    Added Colors and fonts

--- common.py
+++ common.py
@@ -1,4 +1,20 @@
+import pygame
+
+#screen dimensions
 WIDTH = 800  # screen width
 
+#fonts
+font = pygame.font.SysFont("comicsansms", 30)
+smallfont = pygame.font.SysFont("comicsansms", 30)
+
+#colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+slategrey = (112, 128, 144)
+lightgrey = (165, 175, 185)
+blackish = (10, 10, 10)
+
+#load images
+b_king = pygame.image.load('blue_king.png')
+
+
