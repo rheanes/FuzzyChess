0b375f74ee26919ecb866a21897a13508774eec7
roberteanes <rhilleanes@gmail.com>
2022-01-30 20:43:23 -0500

    removed button locations

--- common.py
+++ common.py
@@ -7,10 +7,6 @@ tickrate = 25
 WIDTH = 800  # screen width
 HEIGHT = 800 # screen height
 
-#button locations
-ObjButtonLoc = [WIDTH / 6, HEIGHT / 8, 140, 40]
-RuleButtonLoc = [WIDTH / 2, HEIGHT / 8, 140, 40]
-PieceButtonLoc = [5*WIDTH / 6, HEIGHT / 8, 140, 40]
 
 #fonts
 font = pygame.font.SysFont("comicsansms", 30)
