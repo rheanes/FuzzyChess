6166c91cdb50cf46da866002f544ac65e579010f
Steven Crowther <41662602+Democritus78@users.noreply.github.com>
2022-02-02 20:26:19 -0500

    added color

--- common.py
+++ common.py
@@ -21,6 +21,7 @@ buttoncolor = (165, 175, 185)
 blackish = (10, 10, 10)
 RED = (255, 0, 0) # highlight attack positions
 BLUE = (50, 255, 255) # highlight potential positions
+GREY = (128, 128, 128)
 YELLOW = (204, 204, 0) # highlight pieces controlled by a commanders' pieces
 BROWN = (222, 184, 135)
 LIGHT_GRAY = (211, 211, 211)
