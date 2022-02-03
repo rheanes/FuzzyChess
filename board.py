42deab2d4b257628348dd2b6d3d29df6cb30615e
Steven Crowther <41662602+Democritus78@users.noreply.github.com>
2022-02-02 20:14:02 -0500

    updated board

--- board.py
+++ board.py
@@ -1,48 +1,74 @@
+import pygame
+
 from common import *
-import pieces as pc
+from pieces import Piece, Team, Type
+
+
 class Square:
-    def __init__(self):
+    def __init__(self, piece):
+        super().__init__()
         self.row = 0
         self.col = 0
-        self.x_pos = self.row * (WIDTH // 8)
-        self.y_pos = self.col * (WIDTH // 8)
-        self.color = WHITE
-        self.piece = None
+        # self.x_pos = self.row * (WIDTH // 8)
+        # self.y_pos = self.col * (WIDTH // 8)
+        self.color = (255, 255, 255)
+        self.piece = piece
 
 
-board = [[Square() for _ in range(8)] for _ in range(8)]
+board = [[Square(None) for _ in range(8)] for _ in range(8)]
+
 
 # creates the board
 def create_board():
-    board[0] = [Square(pc.Piece('r', 'r', './images/red_rook.png', 'r')), Square(pc.Piece('r', 'n', './images/orange_knight.png', 'o')),
-                Square(pc.Piece('r', 'b', './images/orange_bishop.png', 'o')), Square(pc.Piece('r', 'q', './images/red_queen.png', 'r')),
-                Square(pc.Piece('r', 'k', './images/red_king.png', 'r')), Square(pc.Piece('r', 'b', './images/yellow_bishop.png', 'y')),
-                Square(pc.Piece('r', 'n', './images/yellow_knight.png', 'y')), Square(pc.Piece('r', 'r', './images/red_rook.png', 'r'))]
+    board[0] = [Square(Piece(Team.RED, Type.ROOK, pygame.image.load('./images/red_rook.png'))),
+                Square(Piece(Team.ORANGE, Type.KNIGHT, pygame.image.load('./images/orange_knight.png'))),
+                Square(Piece(Team.ORANGE, Type.BISHOP, pygame.image.load('./images/orange_bishop.png'))),
+                Square(Piece(Team.RED, Type.QUEEN, pygame.image.load('./images/red_queen.png'))),
+                Square(Piece(Team.RED, Type.KING, pygame.image.load('./images/red_king.png'))),
+                Square(Piece(Team.YELLOW, Type.BISHOP, pygame.image.load('./images/yellow_bishop.png'))),
+                Square(Piece(Team.YELLOW, Type.KNIGHT, pygame.image.load('./images/yellow_knight.png'))),
+                Square(Piece(Team.RED, Type.ROOK, pygame.image.load('./images/red_rook.png')))]
 
-    board[7] = [Square(pc.Piece('b', 'r', './images/blue_rook.png', 'b')), Square(pc.Piece('b', 'n', './images/green_knight.png', 'g')),
-                Square(pc.Piece('b', 'b', './images/green_bishop.png', 'g')), Square(pc.Piece('b', 'q', './images/blue_queen.png', 'b')),
-                Square(pc.Piece('b', 'k', './images/blue_king.png', 'b')), Square(pc.Piece('b', 'b', './images/purple_bishop.png', 'p')),
-                Square(pc.Piece('b', 'n', './images/purple_knight.png', 'p')), Square(pc.Piece('b', 'r', './images/blue_rook.png', 'b'))]
+    board[7] = [Square(Piece(Team.BLUE, Type.ROOK, pygame.image.load('./images/blue_rook.png'))),
+                Square(Piece(Team.GREEN, Type.KNIGHT, pygame.image.load('./images/green_knight.png'))),
+                Square(Piece(Team.GREEN, Type.BISHOP, pygame.image.load('./images/green_bishop.png'))),
+                Square(Piece(Team.BLUE, Type.QUEEN, pygame.image.load('./images/blue_queen.png'))),
+                Square(Piece(Team.BLUE, Type.KING, pygame.image.load('./images/blue_king.png'))),
+                Square(Piece(Team.PURPLE, Type.BISHOP, pygame.image.load('./images/purple_bishop.png'))),
+                Square(Piece(Team.PURPLE, Type.KNIGHT, pygame.image.load('./images/purple_knight.png'))),
+                Square(Piece(Team.BLUE, Type.ROOK, pygame.image.load('./images/blue_rook.png')))]
 
-    for i in range(8):
-        if i < 3:
-            board[1][i] = Square(pc.Piece('r', 'p', 'orange_pawn.png', 'o'))
-            board[6][i] = Square(pc.Piece('b', 'p', 'green_pawn.png', 'g'))
-        if 3 < i < 5:
-            board[1][i] = Square(pc.Piece('r', 'p', 'red_pawn.png', 'r'))
-            board[6][i] = Square(pc.Piece('b', 'p', 'blue_pawn.png', 'b'))
+    for col in range(8):
+        if col < 3:
+            board[1][col] = Square(Piece(Team.ORANGE, Type.PAWN, pygame.image.load('./images/orange_pawn.png')))
+            board[6][col] = Square(Piece(Team.GREEN, Type.PAWN, pygame.image.load('./images/green_pawn.png')))
+
+        if 3 < col < 5:
+            board[1][col] = Square(Piece(Team.RED, Type.PAWN, pygame.image.load('./images/red_pawn.png')))
+            board[6][col] = Square(Piece(Team.BLUE, Type.PAWN, pygame.image.load('./images/blue_pawn.png')))
         else:
-            board[1][i] = Square(pc.Piece('r', 'p', 'yellow_pawn.png', 'y'))
-            board[6][i] = Square(pc.Piece('b', 'p', 'purple_pawn.png', 'p'))
+            board[1][col] = Square(Piece(Team.YELLOW, Type.PAWN, pygame.image.load('./images/yellow_pawn.png')))
+            board[6][col] = Square(Piece(Team.PURPLE, Type.PAWN, pygame.image.load('./images/purple_pawn.png')))
 
-    i, j = 0, 0
-    for b_row in board:
-        for square in b_row:
-            # board_state[i].append(square)
-            if (i + j) % 2 == 1:
-                square.color = LIGHT_GRAY
-            j += 1
-        i += 1
+    for row in range(8):
+        for col in range(8):
+            if (row + col) % 2 == 1:
+                board[row][col].color = GREY
 
-def setup_display():
-    pass
+            board[row][col].row = row
+            board[row][col].col = col
+            """
+            board[row][col].x_pos = col * (WIDTH // 8)
+            board[row][col].y_pos = row * (WIDTH // 8)
+            """
+
+
+def remove_highlights():
+    for row in range(8):
+        for col in range(8):
+            if board[row][col].color == BLUE:
+                if (row + col) % 2 == 1:
+                    board[row][col].color = GREY
+                else:
+                    board[row][col].color = WHITE
+    print('removed highlights')
