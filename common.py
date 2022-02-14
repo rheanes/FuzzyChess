2a8228751055d1d5ee2c05cc5ef0d06c4f05b893
roberteanes <rhilleanes@gmail.com>
2022-02-14 00:43:02 -0500

    added background color. Also switched button color and button hover

--- common.py
+++ common.py
@@ -17,8 +17,8 @@ rulesfont = pygame.font.SysFont("Arial", 18)
 #colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-buttonhover = (112, 128, 144)
-buttoncolor = (165, 175, 185)
+buttoncolor = (112, 128, 144)
+buttonhover = (165, 175, 185)
 blackish = (10, 10, 10)
 RED = (255, 0, 0) # highlight attack positions
 BLUE = (50, 255, 255) # highlight potential positions
@@ -26,6 +26,7 @@ GREY = (128, 128, 128)
 YELLOW = (204, 204, 0) # highlight pieces controlled by a commanders' pieces
 BROWN = (222, 184, 135)
 LIGHT_GRAY = (211, 211, 211)
+BACKGROUND = (128, 0, 32)
 
 #load images
 
