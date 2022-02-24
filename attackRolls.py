e23c3cddfb1ed7dad3c5d19c267691c09dcc4ae8
roberteanes <rhilleanes@gmail.com>
2022-02-23 22:23:53 -0500

    Moved attack rolls to Game Functions File

--- attackRolls.py
+++ attackRolls.py
@@ -1,11 +1,1 @@
-import pygame
-import sys
-import common as cm
-import random
 
-def attack():
-    attackRoll = 0
-    min = 1
-    max = 6
-    attackRoll = random.randint(min, max)
-    return attackRoll
