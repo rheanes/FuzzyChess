50711640cb3121fb0d8b8a044b25cdbd05944f4f
roberteanes <rhilleanes@gmail.com>
2022-02-23 22:12:58 -0500

    Added all highlighting and pathfinding functions to this file. Also added Finding square coords and piece movement here.

--- board.py
+++ board.py
@@ -1,9 +1,10 @@
 import pygame
 
 from common import *
-from pieces import Piece, Team, Type, Value
+from pieces import Piece, Team, Type, Value, enemies, pawn_moves_top, pawn_moves_bottom
 
 
+#----------------BOARD CREATING AND SQUATE CLASS ------------
 class Square:
     def __init__(self, piece):
         super().__init__()
@@ -60,12 +61,46 @@ def create_board():
             board[row][col].x_pos = col * (WIDTH // 8)
             board[row][col].y_pos = row * (WIDTH // 8)
             """
-    
-    
-    
-    
+    #_----------_SQUARE UTILITY-------------
 
+def find_square_coordinates(position: tuple[int, int]):
+    interval = GAME_WIDTH / 8
+    x, y = position
+    row = y // interval
+    col = x // interval
+    return int(row), int(col)
 
+def move_piece(curr_pos: Square, new_pos: Square):
+    board[new_pos.row][new_pos.col].piece = board[curr_pos.row][curr_pos.col].piece
+    board[curr_pos.row][curr_pos.col].piece = None
+    print('pieced moved')
+
+#--------------__SQUARE HIGHLIGHTING AND UNHIGHLIGHTING------------------
+
+# highlight possible moves
+# add 'type: Action' for type of action
+def highlight_moves(positions: tuple[int, int], team: Team):
+    for row, col in positions:
+        if board[row][col].color == BLUE:
+            pass
+        else:
+            if board[row][col].piece is None:
+                board[row][col].color = BLUE
+            elif board[row][col].piece is not None:
+                if board[row][col].piece.team in enemies[team]:
+                    board[row][col].color = BLACK
+                else:
+                    pass
+            else:
+                pass
+
+            """
+            if type is Action.MOVE:
+                board[row][col].color = BLUE
+            elif type is Action.ATTACK:
+                board[row][col].color = RED
+            """
+    print('finished highlighting')
 
 
 
@@ -77,3 +112,57 @@ def remove_highlights():
             else:
                 board[row][col].color = WHITE
     print('removed highlights')
+
+
+#------------------_PATHFINDING STUFF-------------
+
+# Takes an input piece, and determines the maximum movement it can make. This is done through a recursive BFS
+# that goes as far as the piece has movement.
+# Implement a BFS algorithm to check spots around the square, and the spots around the
+# accompanying squares up to the length of which the Rook can move.
+# Essentially, from your position, check the coords (x,y), and then their potential partners
+# up to the maximum movement. If any of the spaces found within the BFS are unoccupied, then
+# we will add it to the list we are making. Afterwards, we append the list to the positions
+#############################################################################################
+#                                  POSITIONS TO CHECK PER SPACE                             #
+#############################################################################################
+#                         (pos - 1, pos + 1) | (pos, pos + 1) | (pos + 1, pos + 1)          #
+#                         (pos - 1, pos)     |  CURR_POS      | (pos + 1, pos)              #
+#                         (pos - 1, pos - 1) | (pos, pos - 1) | (pos + 1, pos - 1)          #
+#############################################################################################
+def maxMovement(maxSpeed: int, iterations: int, position: tuple[int, int], startPos: tuple[int, int], positions=None):
+
+    if positions is None:
+        positions = []
+
+    currRow = position[0]
+    currCol = position[1]
+
+    if (currRow < 0) or (currCol < 0):
+        return
+
+    if (currRow > 7) or (currCol > 7):
+        return
+
+    if iterations > maxSpeed:
+        return
+
+    if (board[currRow][currCol].piece is not None) and position != startPos:
+        positions.append(position)
+        return
+
+    maxMovement(maxSpeed, iterations + 1, (currRow, currCol + 1), startPos, positions)
+    maxMovement(maxSpeed, iterations + 1, (currRow + 1, currCol + 1), startPos, positions)
+    maxMovement(maxSpeed, iterations + 1, (currRow + 1, currCol), startPos, positions)
+    maxMovement(maxSpeed, iterations + 1, (currRow - 1, currCol + 1), startPos, positions)
+    maxMovement(maxSpeed, iterations + 1, (currRow + 1, currCol - 1), startPos, positions)
+    maxMovement(maxSpeed, iterations + 1, (currRow - 1, currCol), startPos, positions)
+    maxMovement(maxSpeed, iterations + 1, (currRow - 1, currCol - 1), startPos, positions)
+    maxMovement(maxSpeed, iterations + 1, (currRow + 1, currCol), startPos, positions)
+
+    if position in positions:
+        return positions
+    else:
+        positions.append(position)
+
+    return positions
