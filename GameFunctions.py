4aa3120ef6e999f126e263aa4cbf221a6572e9fb
rheanes <95715277+rheanes@users.noreply.github.com>
2022-02-23 22:26:50 -0500

    Update and rename attackRolls.py to GameFunctions.py
    Renames to Game Functions

--- GameFunctions.py
+++ GameFunctions.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import common as cm
 import random
-
+ #------------------------------ACTUAL DICE ROLL ---------------------
 def attack():
     attackRoll = 0
     min = 1
