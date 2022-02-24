d99ab4df10bf7694d31e5ac292b9ed7e69a70163
roberteanes <rhilleanes@gmail.com>
2022-02-23 21:24:38 -0500

    Added GameState file that contains GameStates.

---
+++ GameState.py
@@ -0,0 +1,18 @@
+
+class GameState(Enum):
+    Quit = -1
+    Home = 0
+    Play = 1
+    Delegate = 11
+    EndTurn = 12
+    Win = 13
+    Loss = 14
+    Rules = 20
+    HowTo = 21
+    Pieces = 22
+    Pawn = 31
+    Rook = 32
+    Knight = 33
+    Queen = 34
+    Bishop = 35
+    King = 36
