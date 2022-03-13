994cc24f458254a3f2474d7a150aafb77953b84b
btruong117 <ghostofonyx47@gmail.com>
2022-03-13 14:40:27 -0400

    rooks now change color when delegated

--- GameScene.py
+++ GameScene.py
@@ -29,9 +29,13 @@ purple_commander = Commander(purple_pieces, pb)
 player_commanders = [green_commander, blue_commander, purple_commander]
 ai_commanders = [orange_commander, red_commander, yellow_commander]
 
-color_matrix = {Team.BLUE: './Images/blue_pawn.png',
+color_matrix_pawn = {Team.BLUE: './Images/blue_pawn.png',
                 Team.GREEN: './Images/green_pawn.png',
                 Team.PURPLE: './Images/purple_pawn.png'}
+
+color_matrix_rook = {Team.BLUE: './Images/blue_rook.png',
+                Team.GREEN: './Images/green_rook.png',
+                Team.PURPLE: './Images/purple_rook.png'}
 
 delegation_mode = False
 
@@ -138,7 +142,10 @@ def delegate(chosen_square):
             # deligation_count += 1
             print('deligated commander selected')
         if (delegated_piece is not None) and (delegated_commander is not None):
-            delegated_piece.switch_sprite(color_matrix[delegated_commander.leader.team])
+            if delegated_piece.type == Type.PAWN:
+                delegated_piece.switch_sprite(color_matrix_pawn[delegated_commander.leader.team])
+            elif delegated_piece.type == Type.ROOK:
+                delegated_piece.switch_sprite(color_matrix_rook[delegated_commander.leader.team])
             blue_commander.delegate(delegated_piece, delegated_commander)
             human_piece_delegated = True
             action_count += 1
